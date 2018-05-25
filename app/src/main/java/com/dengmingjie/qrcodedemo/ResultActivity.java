3606adf2a0a8b42851053f7b47d091661956218f
邓明杰 <mjdeng@1015shop.com>
2018-05-25 16:59:01 +0800

    Initial commit

---
+++ app/src/main/java/com/dengmingjie/qrcodedemo/ResultActivity.java
@@ -0,0 +1,96 @@
+package com.dengmingjie.qrcodedemo;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.StringUtils;
+import com.google.zxing.decode.DecodeThread;
+
+import java.util.Hashtable;
+
+public class ResultActivity extends Activity {
+
+	private ImageView mResultImage;
+	private TextView mResultText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+
+		Bundle extras = getIntent().getExtras();
+
+		mResultImage = (ImageView) findViewById(R.id.result_image);
+		mResultText = (TextView) findViewById(R.id.result_text);
+
+		if (null != extras) {
+			int width = extras.getInt("width");
+			int height = extras.getInt("height");
+
+			LayoutParams lps = new LayoutParams(width, height);
+			lps.topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, getResources().getDisplayMetrics());
+			lps.leftMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
+			lps.rightMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
+			
+			mResultImage.setLayoutParams(lps);
+
+			String result = extras.getString("result");
+			mResultText.setText(result);
+
+            Bitmap bitmap = null;
+            if (null != extras.getString("QRCode")) {
+                try {
+                    bitmap = Create2DCode(result);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                byte[] compressedBitmap = extras.getByteArray(DecodeThread.BARCODE_BITMAP);
+                if (compressedBitmap != null) {
+                    bitmap = BitmapFactory.decodeByteArray(compressedBitmap, 0, compressedBitmap.length, null);
+                    // Mutable copy:
+                    bitmap = bitmap.copy(Bitmap.Config.RGB_565, true);
+                }
+            }
+
+			mResultImage.setImageBitmap(bitmap);
+		}
+	}
+
+    /**
+     * 用字符串生成二维码
+     */
+    public Bitmap Create2DCode(String str) throws WriterException {
+        Hashtable<EncodeHintType, String> hints = new Hashtable<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        // 生成二维矩阵,编码时指定大小,不要生成了图片以后再进行缩放,这样会模糊导致识别失败
+        BitMatrix matrix = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, 480, 480, hints);
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        // 二维矩阵转为一维像素数组,也就是一直横着排了
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (matrix.get(x, y)) {
+                    pixels[y * width + x] = 0xff000000;
+                }
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        // 通过像素数组生成bitmap,具体参考api
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+}
